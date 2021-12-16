fa77190ed31be345ad36b02a71689e7a5ffa2646
kenanc66 <kenancicekdev@gmail.com>
2021-12-17 01:08:17 +0300

    Data Annotation and Button CSS added

--- ProjeIT/Models/Personnel.cs
+++ ProjeIT/Models/Personnel.cs
@@ -7,7 +7,7 @@ namespace ProjeIT.Models
     {
         public int PersonnelId { get; set; }
         [Required(ErrorMessage ="Please enter your Identity Number")]
-        [StringLength(11,ErrorMessage ="Enter 11 digits please")]
+        [StringLength(11,MinimumLength = 11, ErrorMessage ="Enter 11 digits please")]
         public string IdentityNumber { get; set; }
         [Required]
         public string Name { get; set; }
@@ -18,7 +18,7 @@ namespace ProjeIT.Models
         public string Adress { get; set; }
         public string BirthPlace { get; set; }
         [Required(ErrorMessage = "Please enter your Phone Number")]
-        [StringLength(10, ErrorMessage = "Enter at least 10 digits please")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Enter at least 10 digits please")]
         public string PhoneNumber { get; set; }
         public string SecondaryNumber { get; set; }
         public string BloodType { get; set; }
